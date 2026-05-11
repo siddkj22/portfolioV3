c07fb840dc183d910308148af9f9dddbbd6b0971
Siddharth Jadhav <104563613+siddkj22@users.noreply.github.com>
2026-05-12 03:15:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolioV3/',
+  base: '/portfolioV3/',  // ← Add this line
   plugins: [react()],
 })
-
